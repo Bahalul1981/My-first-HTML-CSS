863f3d4ecadcc5b53fccb0a1cf81f74ab24f0d13
Dawn Amin <92310954+Bahalul1981@users.noreply.github.com>
2021-11-02 17:33:03 +0100

    Add files via upload

---
+++ myhtml.js
@@ -0,0 +1,10 @@
+// myInpute=document.querySelectorAll(".input11")
+
+// function isNumberKey(){
+//     if(myInpute===Number)
+//     return true;
+//     if(myInpute===Text)
+//     return false;
+
+// }
+// isNumberKey();
